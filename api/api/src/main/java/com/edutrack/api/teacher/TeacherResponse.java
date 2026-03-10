dbabc4ce8c833cf37135abb46885669eb3b32c00
Jahir1515 <166346937+Jahir1515@users.noreply.github.com>
2026-03-10 09:53:47 -0500

    Add Teacher Repositoy interface and service CRUD methods

---
+++ api/api/src/main/java/com/edutrack/api/teacher/TeacherResponse.java
@@ -0,0 +1,19 @@
+package com.edutrack.api.teacher;
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class TeacherResponse {
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private Role role;
+  private boolean active;
+}
---
+++ api/api/src/main/java/com/edutrack/api/teacher/TeacherService.java
@@ -0,0 +1,88 @@
+package com.edutrack.api.teacher;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TeacherService {
+  private final TeacherRepository teacherRepository;
+
+  public TeacherResponse create(TeacherRequest request){
+    Teacher teacher = Teacher.builder()
+    .firstName(request.getFirstName())
+    .lastName(request.getLastName())
+    .email(request.getEmail())
+    .password(request.getPassword())
+    .role(request.getRole())
+    .active(true)
+    .build();
+
+    Teacher saved = teacherRepository.save(teacher);
+
+    return TeacherResponse.builder()
+    .id(saved.getId())
+    .firstName(saved.getFirstName())
+    .lastName(saved.getLastName())
+    .email(saved.getEmail())
+    .role(saved.getRole())
+    .active(saved.getActive())
+    .build();
+  }
+
+  public TeacherResponse findById(Long id) {
+    Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new RuntimeException("Profesor no encontrado"));
+
+    TeacherResponse response = TeacherResponse.builder()
+    .id(teacher.getId())
+    .firstName(teacher.getFirstName())
+    .email(teacher.getEmail())
+    .role(teacher.getRole())
+    .active(teacher.getActive())
+    .build();
+
+    return response;
+  }
+
+  public List<TeacherResponse> findAll() {
+    List<Teacher> teachers = teacherRepository.findAll();
+    return teachers.stream()
+                   .map(teacher -> TeacherResponse.builder()
+                    .id(teacher.getId())
+                    .firstName(teacher.getFirstName())
+                    .lastName(teacher.getLastName())
+                    .email(teacher.getEmail())
+                    .role(teacher.getRole())
+                    .active(teacher.getActive())
+                    .build()
+                  )
+                   .collect(Collectors.toList());
+    
+  }  
+
+  public TeacherResponse update(Long id, TeacherRequest request){
+    Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new RuntimeException("Profesor no encontrado"));
+
+    teacher.setFirstName(request.getFirstName());
+    teacher.setLastName(request.getLastName());
+    teacher.setEmail(request.getEmail());
+
+    Teacher update = teacherRepository.save(teacher);
+
+    return TeacherResponse.builder()
+    .id(update.getId())
+    .firstName(update.getFirstName())
+    .lastName(update.getLastName())
+    .email(update.getEmail())
+    .role(update.getRole())
+    .active(update.getActive())
+    .build();
+  }
+
+  public void delete(Long id) {
+    Teacher teacher = teacherRepository.findById(id).orElseThrow(() -> new RuntimeException("Profesor no encontrado"));
+    teacher.setActive(false);
+    teacherRepository.save(teacher);
+  }
+}
